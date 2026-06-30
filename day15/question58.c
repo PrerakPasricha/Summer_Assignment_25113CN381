0afd5cdcb96603f44b7a528483a30eb5523b7863
Prerak Pasricha <pasrichaprerak@gmail.com>
2026-06-30 18:57:19 +0530

    adding day15 questions

---
+++ day15/question58.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int a[100], n, i, first;
+
+    printf("Enter size: ");
+    scanf("%d", &n);
+
+    for(i = 0; i < n; i++)
+        scanf("%d", &a[i]);
+
+    first = a[0];
+
+    for(i = 0; i < n - 1; i++)
+        a[i] = a[i + 1];
+
+    a[n - 1] = first;
+
+    printf("After left rotation:\n");
+
+    for(i = 0; i < n; i++)
+        printf("%d ", a[i]);
+
+    return 0;
+}
