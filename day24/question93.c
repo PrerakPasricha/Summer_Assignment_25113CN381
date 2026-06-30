35eff7c54cac90e4b0f2eda06e3fa7c0693e3d6a
Prerak Pasricha <pasrichaprerak@gmail.com>
2026-06-30 19:07:04 +0530

    adding day24 questions

---
+++ day24/question93.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char s1[100],s2[100],temp[200];
+
+    gets(s1);
+    gets(s2);
+
+    strcpy(temp,s1);
+    strcat(temp,s1);
+
+    if(strstr(temp,s2))
+        printf("Rotation");
+    else
+        printf("Not Rotation");
+
+    return 0;
+}
