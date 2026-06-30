0deadfc4d63ce3ea8d74cb187ae05ec857099605
Prerak Pasricha <pasrichaprerak@gmail.com>
2026-06-30 19:05:51 +0530

    adding day22 questions

---
+++ day22/question87.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    char str[100],ch;
+    int i,count=0;
+
+    gets(str);
+
+    printf("Enter character: ");
+    scanf("%c",&ch);
+
+    for(i=0;str[i]!='\0';i++)
+        if(str[i]==ch)
+            count++;
+
+    printf("Frequency = %d",count);
+
+    return 0;
+}
