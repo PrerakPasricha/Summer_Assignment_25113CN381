42733c58f3115e39e6f51437c900d1f8bc57dd65
Prerak Pasricha <pasrichaprerak@gmail.com>
2026-06-30 18:58:31 +0530

    adding day16 questions

---
+++ day16/question61.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int n, a[100], i, sum = 0;
+
+    printf("Enter value of n: ");
+    scanf("%d", &n);
+
+    printf("Enter %d numbers:\n", n - 1);
+    for(i = 0; i < n - 1; i++) {
+        scanf("%d", &a[i]);
+        sum += a[i];
+    }
+
+    int total = n * (n + 1) / 2;
+
+    printf("Missing Number = %d", total - sum);
+
+    return 0;
+}
---
+++ day16/question62.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    int a[100], n, i, j;
+    int maxFreq = 0, element;
+
+    printf("Enter size: ");
+    scanf("%d", &n);
+
+    for(i = 0; i < n; i++)
+        scanf("%d", &a[i]);
+
+    for(i = 0; i < n; i++) {
+        int count = 1;
+        for(j = i + 1; j < n; j++) {
+            if(a[i] == a[j])
+                count++;
+        }
+
+        if(count > maxFreq) {
+            maxFreq = count;
+            element = a[i];
+        }
+    }
+
+    printf("Element = %d\nFrequency = %d", element, maxFreq);
+
+    return 0;
+}
